d92191ec3bf3be4079048478c7ba3008289307f0
Aaron Kimbrell <awk73@msstate.edu>
2018-04-17 11:58:28 -0500

    Fixed style errors
    Fixed style errors, ignoring line length becuase of the email regex

--- app/retirement.py
+++ app/retirement.py
@@ -1,51 +1,39 @@
-#William Thompson (wtt53)
-#Software Testing and QA
-#Assignment 1: Test Driven Development
-#Retirement: Takes current age (int), annual salary (float),
-#percent of annual salary saved (float), and savings goal (float)
-#and outputs what age savings goal will be met
+# William Thompson (wtt53)
+# Software Testing and QA
+# Assignment 1: Test Driven Development
+# Retirement: Takes current age (int), annual salary (float),
+# percent of annual salary saved (float), and savings goal (float)
+# and outputs what age savings goal will be met
+
 
 def retirement(age, salary, percent, goal):
 
-	try: #cast each variable as its proper data type
-		age = int(age)
-	except:
-		return (False)
+    # cast each variable as its proper data type
+    try:
+        age = int(age)
+        salary = float(salary)
+        percent = float(percent)
+        goal = float(goal)
+    except Exception:
+        return (False)
 
-	try:
-		salary = float(salary)
-	except:
-		return (False)
+    # check each value to make sure it is in the proper range
+    if ((age < 15 or age > 99)
+            or (salary <= 0)
+            or (percent <= 0)
+            or (goal <= 0)):
+        return (False)
+    # savings from salary without employer's 35%
+    rawAnnualSavings = salary * (percent / 100)
+    # total annual savings including employer's 35%
+    annualSavings = rawAnnualSavings + (rawAnnualSavings * 0.35)
+    # total savings so far
+    currentSavings = 0.00
 
-	try:
-		percent = float(percent)
-	except:
-		return (False)
-
-	try:
-		goal = float(goal)
-	except:
-		return (False)
-
-	if age < 15 or age > 99: #check each value to make sure it is in the proper range
-		return (False)
-
-	if salary <= 0:
-		return (False)
-
-	if percent <= 0:
-		return (False)
-
-	if goal <= 0:
-		return (False)
-
-	rawAnnualSavings = salary * (percent / 100) #savings from salary without employer's 35%
-	annualSavings = rawAnnualSavings + (rawAnnualSavings * 0.35) #total annual savings including employer's 35%
-	currentSavings = 0.00 #total savings so far
-
-	for i in range (age, 100): #add annual savings to total savings for each year until age 100
-		currentSavings += annualSavings
-		if currentSavings >= goal:
-			return("You will meet your savings goal at age "+str(i))
-
-	return("Sorry, your goal won't be met.") #Notify user if they will not meet their goal
+    # add annual savings to total savings for each year until age 100
+    for i in range(age, 100):
+        currentSavings += annualSavings
+        if currentSavings >= goal:
+            return("You will meet your savings goal at age "+str(i))
+    # Notify user if they will not meet their goal
+    return("Sorry, your goal won't be met.")
--- app/tip.py
+++ app/tip.py
@@ -1,29 +1,34 @@
-#Jahrell Harris netID: jah1254
-#Software Testing Q&A
-#Spliting Tip amount among guest is what this code's functionality is
-#It accepts only a float value as the tip amount and only an integer value as the guest amount
-#Retruns False when the conditions above are violated and also when any value is 0 for either guest or tip
+# Jahrell Harris netID: jah1254
+# Software Testing Q&A
+# Spliting Tip amount among guest is what this code's functionality is
+# It accepts only a float value as the tip amount and
+# only an integer value as the guest amount
+# Retruns False when the conditions above are violated and
+# also when any value is 0 for either guest or tip
+# Function for splitting tips accepts (float,int)
 
-#Function for splitting tips accepts (float,int)
+
 def split_tip(tip, guest_count):
 
-	#Ensures that given input matches desired input(input does not violate accepted data types)
+    # Ensures that given input matches desired
+    # input(input does not violate accepted data types)
     try:
         float(tip)
-    except Exception as e:
+    except Exception:
         return False
-    try :
+    try:
         int(guest_count)
-    except Exception as e:
+    except Exception:
         return False
 
-    if ((float(tip) == 0) \
-    or (int(guest_count) == 0))\
-    or (float(tip) == round(float(tip)))\
-    or ((float(guest_count)) != round(float(guest_count))):
+    if (((float(tip) == 0)
+            or (int(guest_count) == 0))
+            or (float(tip) == round(float(tip)))
+            or ((float(guest_count)) != round(float(guest_count)))):
         return False
 
-	#If given valid input calculates tip values for each guest rounded to 2 decimal places
+    # If given valid input calculates tip values for each guest
+    # rounded to 2 decimal places
 
     guest_count = int(guest_count)
     tip = float(tip)
