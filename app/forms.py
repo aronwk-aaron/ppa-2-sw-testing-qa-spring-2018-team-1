6e34222950327722c3ab40c47b8a0d212cdf3488
Austin Parker <31323361+ajparker96@users.noreply.github.com>
2018-04-24 15:20:53 -0500

    updated with BMI form

--- app/forms.py
+++ app/forms.py
@@ -8,3 +8,9 @@ class gen_dist_form(FlaskForm):
     x2 = IntegerField('x2:', validators=[DataRequired()])
     y1 = IntegerField('y1:', validators=[DataRequired()])
     y2 = IntegerField('y2:', validators=[DataRequired()])
+
+
+class gen_bmi_form(FlaskForm):
+    f = IntegerField('Feet:', validators=[DataRequired()])
+    i = IntegerField('Inches:', validators=[DataRequired()])
+    p = IntegerField('Pounds:', validators=[DataRequired()])
