af784b7124cc0112bfbbb01d65dee7cae0a56201
Aaron Kimbrell <awk73@msstate.edu>
2018-04-26 01:14:38 -0500

    Added tests for all existing modules
    added test for bmi, email and retirement

--- cypress/integration/e2e_spec.js
+++ cypress/integration/e2e_spec.js
@@ -22,3 +22,66 @@ describe('Testing Distance', function() {
 
   })
 })
+
+describe('Testing e-mail', function() {
+  it('Checks to see if an email is vailid', function(){
+    cy.visit('/email')
+
+    cy.get('input#email_input')
+      .type('ajm712@msstate.edu')
+
+    cy.contains("Submit")
+      .click()
+
+    cy.get('h5')
+      .should('contain', 'Email is Validated')
+
+  })
+})
+
+describe('Testing BMI', function() {
+  it('Calculates BMI given height and weight', function(){
+    cy.visit('/bmi')
+
+    cy.get('input#f')
+      .type('5')
+
+    cy.get('input#i')
+      .type('7')
+
+    cy.get('input#p')
+      .type('150')
+
+    cy.contains("Submit")
+      .click()
+
+    cy.get('h5')
+      .should('contain', 'Your BMI:')
+
+  })
+})
+
+describe('Testing Retirement Savings', function() {
+  it('Calculates the age you will be to save up a certain amount', function(){
+    cy.visit('/retirement')
+
+    cy.get('input#age')
+      .type('23')
+
+    cy.get('input#salary')
+      .type('60000')
+
+    cy.get('input#percent')
+      .type('10')
+
+    cy.get('input#goal')
+      .type('100000')
+
+    cy.contains("Submit")
+      .click()
+
+    cy.get('h5')
+      .should('contain', 'You will meet your savings goal at age 35')
+
+  })
+})
