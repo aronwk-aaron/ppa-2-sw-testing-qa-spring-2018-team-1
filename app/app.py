ce0b6ceac1f36e15c719ed77e289d245cded8672
Aaron Kimbrell <awk73@msstate.edu>
2018-04-30 08:38:35 -0500

    Documentation and jinja2
    just ignore that there is no html now
    Using propper jinja2 file extension

--- app/app.py
+++ app/app.py
@@ -13,7 +13,7 @@ main_blueprint = Blueprint('main', __name__)
 
 @main_blueprint.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.jinja2')
 
 
 @main_blueprint.route('/bmi', methods=['GET', 'POST'])
@@ -26,9 +26,9 @@ def bmi():
         except Exception:
             bmi = -1
 
-        return render_template('bmi.html', form=bmi_form, bmi=bmi, post=1)
+        return render_template('bmi.jinja2', form=bmi_form, bmi=bmi, post=1)
     else:
-        return render_template('bmi.html', form=bmi_form, distance=0, post=0)
+        return render_template('bmi.jinja2', form=bmi_form, distance=0, post=0)
 
 
 @main_blueprint.route('/distance', methods=['GET', 'POST'])
@@ -36,9 +36,9 @@ def distance():
     dist_form = gen_dist_form(request.form)
     if request.method == 'POST':
         distance = calc_distance(dist_form.x1.data, dist_form.y1.data, dist_form.x2.data, dist_form.y2.data)
-        return render_template('distance.html', form=dist_form, distance=distance, post=1)
+        return render_template('distance.jinja2', form=dist_form, distance=distance, post=1)
     else:
-        return render_template('distance.html', form=dist_form, distance=0, post=0)
+        return render_template('distance.jinja2', form=dist_form, distance=0, post=0)
 
 
 @main_blueprint.route('/email', methods=['GET', 'POST'])
@@ -46,9 +46,9 @@ def email():
     email_form = gen_email_form(request.form)
     if request.method == 'POST':
         email = verify_email(email_form.email_input.data)
-        return render_template('email.html', form=email_form, email=email, post=True)
+        return render_template('email.jinja2', form=email_form, email=email, post=True)
     else:
-        return render_template('email.html', form=email_form, email="", post=0)
+        return render_template('email.jinja2', form=email_form, email="", post=0)
 
 
 @main_blueprint.route('/retirement', methods=['GET', 'POST'])
@@ -56,9 +56,9 @@ def retirement():
     retire_form = gen_retire_form(request.form)
     if request.method == 'POST':
         age = calc_retirement(retire_form.age.data, retire_form.salary.data, retire_form.percent.data, retire_form.goal.data)
-        return render_template('retirement.html', form=retire_form, age=age, post=1)
+        return render_template('retirement.jinja2', form=retire_form, age=age, post=1)
     else:
-        return render_template('retirement.html', form=retire_form, age=0, post=0)
+        return render_template('retirement.jinja2', form=retire_form, age=0, post=0)
 
 
 @main_blueprint.route('/tip', methods=['GET', 'POST'])
@@ -75,6 +75,6 @@ def tip():
                 checks.append(temp_dict)
                 guest += 1
 
-        return render_template('tip.html', form=tip_form, bills=checks, post=1)
+        return render_template('tip.jinja2', form=tip_form, bills=checks, post=1)
     else:
-        return render_template('tip.html', form=tip_form, bills=0, post=0)
+        return render_template('tip.jinja2', form=tip_form, bills=0, post=0)
